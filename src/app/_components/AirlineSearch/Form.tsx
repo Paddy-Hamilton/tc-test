d1c3353ef3f0ee52e7c37313e14daa9e96239d4b
Paddy Hamilton <paddy@paddyhamilton.com>
2024-01-15 17:36:20 +0000

    chore: add comment

--- src/app/_components/AirlineSearch/Form.tsx
+++ src/app/_components/AirlineSearch/Form.tsx
@@ -7,11 +7,15 @@ type Props = {
 };
 
 const Form = ({updateAirlineList}: Props) => {
+  //  THis is the part i was missing. Wrapping the server action searchAirlinesAction in a client call that could
+  // and just returning the airlines from the server action.
   const handleFormAction = async (formData: FormData) => {
     const airlines = await searchAirlinesAction(formData);
-
     updateAirlineList(airlines);
   };
+
+  // if i was todo this again i would have used search params that the input would have updated so that the search results could have been shared and so that going back would have shown previous results
+  // if that was a requirment :)
   return (
     <form action={handleFormAction}>
       <label>Search prefered airline</label>
